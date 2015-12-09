7a635bfa8b66e103931f40412139aefe80d72655
Tobias Bleckert <hola@tobiasbleckert.se>
2015-12-09 18:54:29 +0100

    Another test

--- __tests__/SelectSearch-test.js
+++ __tests__/SelectSearch-test.js
@@ -7,19 +7,27 @@ import TestUtils from 'react-addons-test-utils';
 
 const SelectSearch = require('../index').default;
 
-describe('SelectSearch', () => {
+describe('SelectWithSearch', () => {
+
+    const selectSearch = TestUtils.renderIntoDocument(
+        <SelectSearch name="country" options={[{name: 'Sweden', value: 'SE'}, {name: 'Italy', value: 'IT'}]} value="SE" />
+    );
 
     it('has select options', () => {
-        var selectSearch = TestUtils.renderIntoDocument(
-            <SelectSearch name="country" options={[{name: 'Sweden', value: 'SE'}, {name: 'Italy', value: 'IT'}]} value="SE" />
-        ), selectSearchNode;
+        var selectSearchNode = ReactDOM.findDOMNode(selectSearch);
 
-        selectSearchNode = ReactDOM.findDOMNode(selectSearch);
-
-        TestUtils.Simulate.click(selectSearchNode.querySelector('.select-search-box__search'));
+        TestUtils.Simulate.focus(selectSearchNode.querySelector('.select-search-box__search'));
 
         expect(selectSearchNode.querySelectorAll('.select-search-box__option').length).toEqual(2);
+    });
 
+    it('empties search field on focus', () => {
+        var selectSearchNode = ReactDOM.findDOMNode(selectSearch),
+            searchField      = selectSearchNode.querySelector('.select-search-box__search');
+
+        TestUtils.Simulate.focus(searchField);
+
+        expect(searchField.value.length).toEqual(0);
     });
 
 });
