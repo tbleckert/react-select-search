1825e01a14b88526a937bbb786fb2a06f60f8531
Tobias Bleckert <hola@tobiasbleckert.se>
2018-05-06 01:44:43 +0200

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -1,70 +1,109 @@
-'use strict';
-
 import React from 'react';
-import ReactDom from 'react-dom';
+import { render } from 'react-dom';
 import SelectSearch from './react-select-search';
 
-var countryElement  = document.getElementById('countrySelect'),
-    languageElement = document.getElementById('languageSelect'),
-    friendsElement   = document.getElementById('friendsSelect'),
-    languageOptions = [
-        {name: 'Swedish', value: 'sv'},
-        {name: 'English', value: 'en'},
-        {name: 'Italian', value: 'it'}
-    ],
-    // https://randomuser.me/
-    friends = [
-        {name: 'Annie Cruz', value: 'annie.cruz', photo: 'https://randomuser.me/api/portraits/women/60.jpg'},
-        {name: 'Eli Shelton', value: 'eli.shelton', photo: 'https://randomuser.me/api/portraits/men/7.jpg'},
-        {name: 'Loretta Rogers', value: 'loretta.rogers', photo: 'https://randomuser.me/api/portraits/women/51.jpg'},
-        {name: 'Lloyd Fisher', value: 'lloyd.fisher', photo: 'https://randomuser.me/api/portraits/men/34.jpg'},
-        {name: 'Tiffany Gonzales', value: 'tiffany.gonzales', photo: 'https://randomuser.me/api/portraits/women/71.jpg'},
-    ],
-    /** https://gist.github.com/Keeguon/2310008 */
-    countryOptions = require('./data/countries.json'), _optionsParent = null, _optionsParentHeight = null, _optionHeight = null;
+const countryElement  = document.getElementById('countrySelect');
+const fontElement = document.getElementById('fontSelect');
+const friendsElement   = document.getElementById('friendsSelect');
+const fontStacks = [
+    {name: 'Roboto', value: 'Roboto', 'data-stack': 'Roboto, sans-serif'},
+    {name: 'Playfair Display', value: 'Playfair Display', 'data-stack': '"Playfair Display", serif'},
+    {name: 'Monoton', value: 'Monoton', 'data-stack': 'Monoton, cursive'},
+    {name: 'Gloria Hallelujah', value: 'Gloria Hallelujah', 'data-stack': '"Gloria Hallelujah", cursive'},
+    {name: 'VT323', value: 'VT323', 'data-stack': 'VT323, monospace'}
+];
+// https://randomuser.me/
+const friends = [
+    {name: 'Annie Cruz', value: 'annie.cruz', photo: 'https://randomuser.me/api/portraits/women/60.jpg'},
+    {name: 'Eli Shelton', value: 'eli.shelton', photo: 'https://randomuser.me/api/portraits/men/7.jpg'},
+    {name: 'Loretta Rogers', value: 'loretta.rogers', photo: 'https://randomuser.me/api/portraits/women/51.jpg'},
+    {name: 'Lloyd Fisher', value: 'lloyd.fisher', photo: 'https://randomuser.me/api/portraits/men/34.jpg'},
+    {name: 'Tiffany Gonzales', value: 'tiffany.gonzales', photo: 'https://randomuser.me/api/portraits/women/71.jpg'},
+];
+/** https://gist.github.com/Keeguon/2310008 */
+const countryOptions = require('./data/countries');
+
+let fontStack;
 
 /** Render selectbox */
-ReactDom.render(
-    <SelectSearch name="language" value="sv" options={languageOptions} search={false} placeholder="Choose language" />,
-    languageElement
+render(
+    <SelectSearch name="font" value="Playfair Display" renderOption={renderFontOption} onChange={onSelectChange} onBlur={onBlur} onMount={onBlur} options={fontStacks} search={false} placeholder="Choose font" />,
+    fontElement
 );
 
-function onMount(value) {
-    console.log('Mount', value);
+function getSearchElement(props) {
+    let outElement = document.querySelector('[name="' + props.name + '"]');
+
+    if (!outElement) {
+        return null;
+    }
+
+    return outElement.nextSibling;
+}
+
+function setFontStackOnSearch(value, props) {
+    let search = getSearchElement(props);
+
+    if (search) {
+        search.style.fontFamily = value['data-stack'];
+    }
+}
+
+function clearFontStackOnSearch(value, props) {
+    let search = getSearchElement(props);
+
+    if (search) {
+        search.style.fontFamily = '';
+    }
 }
 
 function onChange(value, state, props) {
-    console.log('Change', value);
-}
-
-function onHighlight(value, state, props) {
-    console.log('Highlight', value);
+    fontStack = option['data-stack'];
+    this.refs.search.style.fontFamily = fontStack;
 }
 
 function onBlur(value, state, props) {
-    console.log('Blur', value);
+    setFontStackOnSearch(value, props);
 }
 
-function onFocus(value, state, props) {
-    console.log('Focus', value);
+function renderFontOption (option) {
+    let style = {
+        fontFamily: option['data-stack']
+    };
+
+    return <span style={style}>{option.name}</span>;
 }
 
-ReactDom.render(
+function onSelectChange(value, state, props) {
+    let outElement = document.querySelector('[name="' + props.name + '"]');
+
+    if (outElement) {
+        if ('createEvent' in document) {
+            const event = document.createEvent('HTMLEvents');
+            event.initEvent('change', true, true);
+            outElement.dispatchEvent(event);
+        } else {
+            outElement.fireEvent('onchange');
+        }
+    }
+
+    if (value.hasOwnProperty('data-stack')) {
+        setFontStackOnSearch(value, props);
+    }
+}
+
+render(
     <SelectSearch
         name="country"
         mode="input"
+        value="SE"
         options={countryOptions}
-        onMount={onMount}
-        onChange={onChange}
-        onHighlight={onHighlight}
-        onBlur={onBlur}
-        onFocus={onFocus}
         placeholder="Your country" />,
     countryElement
 );
 
 function renderFriend(option) {
-    let imgStyle = {
+    const imgStyle = {
         borderRadius: '50%',
         verticalAlign: 'middle',
         marginRight: 10
@@ -73,7 +112,7 @@ function renderFriend(option) {
     return (<span><img style={imgStyle} width="40" height="40" src={option.photo} /><span>{option.name}</span></span>);
 }
 
-ReactDom.render(
+render(
     <SelectSearch name="friends" multiple={true} height={172} options={friends} placeholder="Search friends" renderOption={renderFriend} />,
     friendsElement
 );
