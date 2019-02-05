fa2dac7c35d9f4654f1a03857d81c2f0541da686
coxdn <cxdnhd@gmail.com>
2019-02-05 12:01:59 +0200

    slightly more tests

--- __tests__/selectsearch.test.js
+++ __tests__/selectsearch.test.js
@@ -2,34 +2,45 @@ import test from 'ava';
 import React from 'react';
 import { mount } from 'enzyme';
 import SelectSearch from '../src';
+import { countries, friends } from './data';
 
-const options = [{ name: 'Sweden', value: 'SE' }, { name: 'Italy', value: 'IT' }];
+
+const component = mount(<SelectSearch name="country" options={countries} value="SE" />);
 
 test('has search field', (t) => {
-    const component = mount(<SelectSearch name="country" options={options} value="SE" />);
-
     t.is(component.find('.select-search-box__search').length, 1);
 });
 
-test('displays options on focus', (t) => {
-    const component = mount(<SelectSearch name="country" options={options} value="SE" />);
+test('default selected should be Sweden', (t) => {
+    t.is(component.find('.select-search-box__search').props().value, 'Sweden');
+});
 
+test('displays options on focus', (t) => {
     component.find('.select-search-box__search').simulate('focus');
+    
+    t.is(component.find('.select-search-box__option').length, 243);
+});
+
+test('search field on focus should be empty', (t) => {
+    t.is(component.find('.select-search-box__search').props().value, '');
+});
+
+const search = 'Ital';
+
+test('empties search field on focus', (t) => {
+    component.find('.select-search-box__search').simulate('change', { target: { value: search } });
 
     t.is(component.find('.select-search-box__option').length, 2);
 });
 
-test('empties search field on focus', (t) => {
-    const component = mount(<SelectSearch name="country" options={options} value="SE" />);
-    const search = 'Italy';
-
-    t.is(component.find('.select-search-box__search').props().value, 'Sweden');
-
-    component.find('.select-search-box__search').simulate('focus');
-
-    t.is(component.find('.select-search-box__search').props().value, '');
-
-    component.find('.select-search-box__search').simulate('change', { target: { value: search } });
-
+test('not empties search field after entering', (t) => {
     t.is(component.find('.select-search-box__search').props().value, search);
+});
+
+test('select another option', (t) => {
+    component.find('.select-search-box__search').simulate('blur');
+    component.find('.select-search-box__search').simulate('focus');
+    component.find('.select-search-box__option[data-value="BH"]').simulate('click');
+    
+    t.is(component.find('.select-search-box__search').props().value, 'Bahrain');
 });
