ad11e8ffc1541fe220f2a3c7a4436201249c463e
Tobias Bleckert <hola@tobiasbleckert.se>
2020-05-10 13:35:52 +0200

    Dependency fixes and storybook source

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-storysource'],
   webpackFinal: async config => {
     // do mutation to the config
 
