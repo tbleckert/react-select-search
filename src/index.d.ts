ff389b3c49b7671ce2f38014c67c67c664b95614
Mohamed Feddad <mrf345@gmail.com>
2020-05-27 15:13:28 +0300

    Refactor renderOption, valueOption args and types. Add new props.

--- src/index.d.ts
+++ src/index.d.ts
@@ -11,22 +11,66 @@ export type SelectSearchOption = {
     type?:string
     items?:SelectSearchOption[]
     disabled?:boolean
+    photo?:string
 }
 
-export type SelectSearchReturnedOption = {
+export type SelectedOptionValue = {
     name:string
     value:string
     index:number
+    photo?:string
+    disabled?:boolean
 }
 
-export type SelectSearchState = {
-    defaultOptions:SelectSearchReturnedOption[][]
+export type SelectedOption = {
+    defaultOptions:SelectedOptionValue[][]
     focus:boolean
-    highlighted:null|SelectSearchReturnedOption
-    options:SelectSearchReturnedOption[][]
+    highlighted:null|SelectedOptionValue
+    options:SelectedOptionValue[][]
     search:string
     value:string
 }
+
+export type OptionSnapshot = {
+    selected:boolean
+    highlighted:boolean
+}
+
+
+export type DomProps = {
+    tabIndex:string
+    onMouseDown:(event:MouseEvent) => void
+    onKeyDown:(event:KeyboardEvent) => void
+    onKeyPress:(event:KeyboardEvent) => void
+    onBlur:(event:Event) => void
+    value:string
+    disabled:boolean
+}
+
+export type ValueProps = {
+    tabIndex:string
+    readonly:boolean
+    onMouseDown:(event:MouseEvent) => void
+    onKeyDown:(event:KeyboardEvent) => void
+    onKeyUp:(event:KeyboardEvent) => void
+    onKeyPress:(event:KeyboardEvent) => void
+    onBlur:(event:Event) => void
+    value:string
+    disabled:boolean
+}
+
+export type ValueSnapshot = {
+    value:SelectedOptionValue
+    highlighted:boolean
+    options:SelectedOptionValue[]
+    disabled:boolean
+    displayValue:string
+    focus:boolean
+    search:string
+    searching:boolean
+}
+
+export type PrintOptions = 'auto'|'always'|'never'|'on-focus'
 
 export type SelectSearchProps = {
     options:SelectSearchOption[]
@@ -38,11 +82,14 @@ export type SelectSearchProps = {
     autoComplete?:'on'|'off'
     autoFocus?:boolean
     fuse?:FuseOption|boolean
-    className?:(classes:string) => string|string
-    onChange?:(optionProps:SelectSearchReturnedOption, optionData:SelectSearchState, optionSnapshot:SelectSearchProps) => void
-    renderOption?:(optionProps:SelectSearchReturnedOption, optionData:SelectSearchState, optionSnapshot:SelectSearchProps) => React.ReactNode
-    renderValue?:(valueProps:any, ref:React.Ref<React.Component>, selectedValue:any) => React.ReactNode
+    className?:((classes:string) => string)|string
+    onChange?:(selectedValue:SelectedOptionValue|SelectedOptionValue[], selectedOption:SelectedOption|SelectedOption[], optionSnapshot:SelectSearchProps) => void
+    printOptions?:PrintOptions
+    closeOnSelect?:boolean
+    renderOption?:(domProps:DomProps, option:SelectedOption, snapshot:OptionSnapshot, className:string) => React.ReactNode
+    renderValue?:(valueProps:ValueProps, snapshot:ValueSnapshot, className:string) => React.ReactNode
     renderGroupHeader?:(name:string) => string
+    getOptions?:(query:string) => Promise<SelectedOption>
     ref?:React.Ref<React.Component>
 }
 
