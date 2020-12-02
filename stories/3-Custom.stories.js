9a030c165aa33e207161af774c60ba702818d987
Tobias Bleckert <hola@tobiasbleckert.se>
2020-12-02 17:05:08 +0100

    Example with printOptions always/never

--- stories/3-Custom.stories.js
+++ stories/3-Custom.stories.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import SelectSearch from '../src';
 import '../style.css';
 import classes from '../style.module.css';
@@ -71,3 +72,39 @@ export const CSSModules = () => (
         ]}
     />
 );
+
+const button = {
+    marginLeft: '16px',
+    display: 'inline-flex',
+    position: 'relative',
+    alignItems: 'center',
+    height: '37px',
+    padding: '0 16px',
+    borderRadius: '3px',
+    border: 'none',
+    background: 'rgb(49, 173, 122)',
+    color: '#fff',
+    fontSize: '16px',
+    cursor: 'pointer',
+    outline: 'none',
+};
+
+export const ControllableDisplay = () => {
+    const [displayOptions, setDisplayOptions] = useState(false);
+
+    return (
+        <div style={{ display: 'flex' }}>
+            <SelectSearch
+                printOptions={(displayOptions) ? 'always' : 'never'}
+                options={[
+                    { value: 'hamburger', name: 'Hamburger' },
+                    { value: 'fries', name: 'Fries' },
+                    { value: 'milkshake', name: 'Milkshake' },
+                ]}
+            />
+            <button type="button" style={button} onClick={() => setDisplayOptions(!displayOptions)}>
+                {!displayOptions ? 'Display options' : 'Hide options'}
+            </button>
+        </div>
+    );
+};
