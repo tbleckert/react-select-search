967132c4b2e8df34fdbf71a6c5ff361f24d95862
Tobias Bleckert <hola@tobiasbleckert.se>
2020-12-13 01:25:33 +0100

    Fixed jest css modules problem

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/{!(types.js|index.js),}.{js,jsx}"],
-  testMatch: ["<rootDir>/__tests__/*.test.{js,jsx}"],
+    collectCoverageFrom: ['src/**/{!(types.js|index.js),}.{js,jsx}'],
+    testMatch: ['<rootDir>/__tests__/*.test.{js,jsx}'],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
 };
