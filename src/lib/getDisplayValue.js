cf96930fec1acc24bfe3efc01bfcdbcf463c42a9
xfayne <40721408+xfayne@users.noreply.github.com>
2021-05-04 17:24:01 +0300

    Update getDisplayValue.js

--- src/lib/getDisplayValue.js
+++ src/lib/getDisplayValue.js
@@ -2,7 +2,7 @@ import isOption from './isOption';
 
 export default function getDisplayValue(value) {
     if (Array.isArray(value)) {
-        return value.map((o) => isOption(o) && o.name).join(', ');
+        value.length? value.map(o => isOption(o) && o.name).join(', '): 'Add new trip';
     }
 
     return isOption(value) ? value.name : '';
