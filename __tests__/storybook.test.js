73c63bac20f0d5f340206159bb9c08254289fab8
Joey Paris <joey@leadjig.com>
2021-05-21 17:16:53 -0400

    Add additional storybook testing

--- __tests__/storybook.test.js
+++ __tests__/storybook.test.js
@@ -1,3 +1,35 @@
-import initStoryShots from '@storybook/addon-storyshots';
+import React from 'react'
+import initStoryShots, { Stories2SnapsConverter } from '@storybook/addon-storyshots';
+import { create, act } from 'react-test-renderer';
+import "regenerator-runtime/runtime";
+import fetchMock from "jest-fetch-mock"
 
-initStoryShots();
+fetchMock.enableMocks()
+fetch.mockResponse(JSON.stringify({ drinks: [] }))
+
+const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
+
+const converter = new Stories2SnapsConverter();
+
+initStoryShots({
+    asyncJest: true,
+    test: async ({ story, context, done }) => {
+        let renderer;
+        act(() => {
+            // React.createElement() is important because of hooks [shouldn't call story.render() directly]
+            renderer = create(React.createElement(story.render), {
+                // Fix Portal / MUI Dialog issues
+                createNodeMock: (node) => document.createElement(node.type),
+            });
+        });
+
+        // Let one render cycle pass before rendering snapshot
+        await act(() => wait(0));
+
+        // save each snapshot to a different file (similar to "multiSnapshotWithOptions")
+        const snapshotFileName = converter.getSnapshotFileName(context);
+        expect(renderer).toMatchSpecificSnapshot(snapshotFileName);
+
+        done();
+    },
+});
--- stories/4-Async.stories.js
+++ stories/4-Async.stories.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import SelectSearch from '../src';
 import '../style.css';
 import { countries, fontStacks } from './data';
@@ -42,3 +43,66 @@ export const FetchMultiple = () => (
         placeholder="Your favorite drink"
     />
 );
+
+export const ControlledFetch = () => {
+    const [options, setOptions] = useState([])
+    const [value, setValue] = useState()
+    const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        if (!options.length || query) {
+            fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+                .then(response => response.json())
+                .then(({ drinks }) => {
+                    setOptions(drinks.map(({ idDrink, strDrink }) => ({ value: idDrink, name: strDrink })))
+                })
+                .catch((e) => console.error(e));
+        }
+    }, [query])
+
+    return(
+        <SelectSearch
+            options={options}
+            value={value}
+            onChange={setValue}
+            getOptions={(query) => {
+                setQuery(query)
+            }}
+            search
+            placeholder="Your favorite drink"
+        />
+    )
+};
+
+export const ControlledFetchMultiple = () => {
+    const [options, setOptions] = useState([])
+    const [value, setValue] = useState([])
+    const [valueOptions, setValueOptions] = useState([])
+    const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+            .then(response => response.json())
+            .then(({ drinks }) => {
+                setOptions([...valueOptions, ...drinks.map(({ idDrink, strDrink }) => ({ value: idDrink, name: strDrink }))])
+            })
+            .catch((e) => console.error(e));
+    }, [query])
+
+    return(
+        <SelectSearch
+            options={options}
+            value={value}
+            onChange={(value) => {
+                setValue(value)
+                setValueOptions(options.filter((o) => value.includes(o.value)))
+            }}
+            multiple
+            getOptions={(query) => {
+                setQuery(query)
+            }}
+            search
+            placeholder="Your favorite drink"
+        />
+    )
+};
