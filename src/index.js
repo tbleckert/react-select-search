c4041a62996f9a0c782afc5b7ce38d34e7c867bd
Tobias Bleckert <hola@tobiasbleckert.se>
2022-06-21 20:33:23 +0200

    No need to export fuzzySearch anymore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
+import SelectSearch from './SelectSearch';
 export { default as useSelect } from './useSelect';
-// eslint-disable-next-line no-restricted-exports
-export { default } from './SelectSearch';
-export { default as fuzzySearch } from './fuzzySearch';
+
+export default SelectSearch;
