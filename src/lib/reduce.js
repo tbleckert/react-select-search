431f87d6c972d694bc315554325025465429b2a8
Tobias Bleckert <hola@tobiasbleckert.se>
2022-06-22 10:51:12 +0200

    Bugfix

--- src/lib/reduce.js
+++ src/lib/reduce.js
@@ -2,5 +2,5 @@ export default function reduce(middleware, items, query) {
     return middleware.filter((cb) => typeof cb === 'function').reduce(
         (data, cb) => cb(data, query),
         items
-    );
+    ).map((item, i) => ({ ...item, index: i }));
 }
