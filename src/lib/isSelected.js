1b599972fc6b17232b20f9aec9ac959cd6c45ccc
Seif-apprentus <seif@apprentus.com>
2023-12-12 13:52:13 +0100

    compare values instead of objects to fix selected option

--- src/lib/isSelected.js
+++ src/lib/isSelected.js
@@ -2,6 +2,8 @@ export default function isSelected(itemValue, selectedValue) {
     return (
         (Array.isArray(selectedValue)) ?
             selectedValue.findIndex(item => item.value === itemValue.value) >= 0
-            : selectedValue === itemValue
+            : selectedValue
+            ? selectedValue.value === itemValue.value
+            : false
     );
 }
