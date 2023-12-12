f2f24557d881d7cd4a3aecbf4d4adb8b9cc7bfc6
Seif-apprentus <seif@apprentus.com>
2023-12-12 14:11:38 +0100

    push new builds

--- dist/esm/lib/isSelected.js
+++ dist/esm/lib/isSelected.js
@@ -1,3 +1,3 @@
 export default function isSelected(itemValue, selectedValue) {
-  return Array.isArray(selectedValue) ? selectedValue.findIndex(item => item.value === itemValue.value) >= 0 : selectedValue === itemValue;
+  return Array.isArray(selectedValue) ? selectedValue.findIndex(item => item.value === itemValue.value) >= 0 : selectedValue ? selectedValue.value === itemValue.value : false;
 }
